9d1bc87e0672291ac3a359de8ace15fb590bf5c3
ZigaRajbar <ziga.rajbar3@gmail.com>
2026-08-26 19:59:23 +0200

    Dodelana analiza.ipynb in readme.md, zakljucni dodatki urejanja podatkov in popravljene slovnične napake

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from pridobi_podatke import raw_podatki_s_spletne_strani
 from uredi_podatke import uredi_raw_podatke, naredi_csv
 
-stevilo_strani = 100
 
-print(naredi_csv(uredi_raw_podatke(raw_podatki_s_spletne_strani(stevilo_strani))))
+def main():
+    stevilo_strani = 200
+
+    print("Pridobivanje podatkov...")
+    raw_podatki = raw_podatki_s_spletne_strani(stevilo_strani)
+
+    print("Urejanje podatkov...")
+    urejeni_podatki = uredi_raw_podatke(raw_podatki)
+
+    print(f"Najdenih avtomobilov: {len(urejeni_podatki)}")
+
+    naredi_csv(urejeni_podatki)
+    print("Podatki uspešno urejeni!")
+
+
+main()
